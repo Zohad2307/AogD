91f5a2b54701de75bcf9f0088ec534d126309148
Zohad2307 <adel1086@stud.kea.dk>
2021-09-29 10:46:06 +0200

    "Øvelse med abu Ali Musa"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// Hello World kommentar
         String HelloWorld;
         System.out.println("Hello World A og D");
+        Scanner in = new Scanner(System.in);
+        HelloWorld = in.nextLine();
+        System.out.println("Du har indtastet " + HelloWorld);
     }
 }
