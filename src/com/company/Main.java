5c8661622fedaa490ea2b9ebcb7c768897cf1823
Zohad2307 <adel1086@stud.kea.dk>
2021-09-29 10:30:03 +0200

    "Øvelse med abu Ali Musa"

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Hello World kommentar
+        String HelloWorld;
+        System.out.println("Hello World A og D");
+    }
+}
